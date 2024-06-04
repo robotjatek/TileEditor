56856c02a717747c6eae624b196177f2b0d7bb63
Attila Szücs <ati.szucs@hotmail.com>
2024-06-05 00:13:08 +0200

    MainWindow style

--- TileEditor/MainWindow.xaml.cs
+++ TileEditor/MainWindow.xaml.cs
@@ -1,25 +1,29 @@
-﻿using System.Text;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using System.Windows;
 
-namespace TileEditor
+namespace TileEditor;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
-    public partial class MainWindow : Window
+    public MainWindow()
     {
-        public MainWindow()
-        {
-            InitializeComponent();
-            DataContext = new MainWindowViewModel();
-        }
+        InitializeComponent();
+    }
+
+    private void Close_Click(object sender, RoutedEventArgs e)
+    {
+        Close();
+    }
+
+    private void Resize_Click(object sender, RoutedEventArgs e)
+    {
+        this.WindowState ^= WindowState.Maximized;
+    }
+
+    private void Minimize_Click(object sender, RoutedEventArgs e)
+    {
+        this.WindowState = WindowState.Minimized;
     }
 }
